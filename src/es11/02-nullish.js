f35b2f602b15ced217063088441bae9bee20db0c
serghip <piedrabuena.sergio.f@gmail.com>
2022-11-02 08:44:46 -0300

    ecma 11

--- src/es11/02-nullish.js
+++ src/es11/02-nullish.js
@@ -1,4 +1,4 @@
 // ?? es nullish. comprueba si un valor es null o undefined, en cuyo caso muestra o elige otro camino.
 const anotherNumber = null;
-const validate = anotherNumber ?? 5;
+const validate = anotherNumber ?? 5; 
 console.log(validate)
---
+++ src/es11/04-globalthis.js
@@ -0,0 +1,6 @@
+console.log(window)//en navegador muestra distintos metodos y elementos del mismo
+console.log(global)//misma opcion del lado de node
+console.log(selft)//webworker
+
+
+console.log(globalThis)//contexto global
